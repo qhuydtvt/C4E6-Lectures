f21dc5b571cdc3bda3b61eeb25e69d961e7eed9a
qhuydtvt <qhuydtvt@gmail.com>
2016-09-05 22:50:13 +0700

    Session1

---
+++ Session1/1.py
@@ -0,0 +1,13 @@
+print('Hello world')
+
+n = input("what's your name?")
+n = "ahfuehf"
+print('Hello ', n)
+
+
+y = int(input('your year of birth?'))
+
+age = 2016 - y
+print(age)
+
+
---
+++ Session1/2.py
@@ -0,0 +1,18 @@
+from turtle import *
+shape("turtle")
+
+speed(-1)
+
+for i in range(10):
+    forward (100)
+    left (90)
+    forward (100)
+    left (90)
+    forward (100)
+    left (90)
+    forward (100)
+    left (7)
+    print("Hi")
+
+print("Hello")
+mainloop ()
